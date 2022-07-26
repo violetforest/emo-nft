446bb4b1563295ae3208c46a20135a4ce894c901
DESKTOP-Q8RBGJI\Violeta <violetforest5@protonmail.com>
2022-07-26 18:22:06 -0400

    add bgs

--- src/colors.js
+++ src/colors.js
@@ -5,9 +5,11 @@ let colors = [
     {bg: 'neonskull.jpg', color: '#ffb6da', bg2: 'black.jpg', repeat: 'repeat', bgSize: '15%', bgAttach: 'scroll'},
     {bg: 'tinyheart.jpg', color: '#fe98ff', bg2: 'black.jpg', repeat: 'repeat', bgSize: '10%', bgAttach: 'scroll'},
     {bg: 'blinkheart.gif', color: '#80ff80', bg2: 'black.jpg', repeat: 'repeat', bgSize: '20%', bgAttach: 'scroll'},
-    {bg: 'purpleskull.jpg', color: '#fff', bg2: 'purpleskull.jpg', repeat: 'repeat', bgSize: '30%', bgAttach: 'scroll'},
+    {bg: 'purpleskull.jpg', color: '#fff', bg2: 'black.jpg', repeat: 'repeat', bgSize: '30%', bgAttach: 'scroll'},
     {bg: 'pinkglitter.jpg', color: '#fff', bg2: 'black.jpg', repeat: 'repeat', bgSize: '20%', bgAttach: 'scroll'},
-    {bg: 'swirlstars.jpg', color: '#fff', bg2: 'black.jpg', repeat: 'repeat', bgSize: '100%', bgAttach: 'fixed'}
+    {bg: 'swirlstars.jpg', color: '#c0a9ff', bg2: 'black.jpg', repeat: 'repeat', bgSize: '100%', bgAttach: 'fixed'},
+    {bg: 'glitterskulls.gif', color: '#fff', bg2: 'black.jpg', repeat: 'repeat', bgSize: '15%', bgAttach: 'scroll'},
+    {bg: 'pinkhearts.gif', color: '#ffc6f1', bg2: 'black.jpg', repeat: 'repeat', bgSize: '15%', bgAttach: 'scroll'}
 ]
 
 let randomColor = colors[Math.floor(Math.random() * colors.length)];
